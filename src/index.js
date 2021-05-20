039827854a87a2adffdd0acd8e35e0b49309be62
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-05-20 14:04:50 +0300

    init project

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import "./scss/index.scss";
+
+
+/* SCROLL TO TOP BTN */
+const scrollToTopButton = document.getElementById("js-top");
+const scrollFunc = () => {
+  const y = window.scrollY;
+  if (y > 200) {
+    scrollToTopButton.className = "top-link show";
+  } else {
+    scrollToTopButton.className = "top-link hide";
+  }
+};
+
+window.addEventListener("scroll", scrollFunc);
+
+const scrollToTop = () => {
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / 18);
+  }
+};
+
+scrollToTopButton.onclick = function (e) {
+  e.preventDefault();
+  scrollToTop();
+};
+/* burger */
+(function () {
+  const hamburger = {
+    navToggle: document.querySelector('.nav-toggle'),
+    nav: document.querySelector('nav'),
+
+    doToggle: function (e) {
+      e.preventDefault();
+      this.navToggle.classList.toggle('expanded');
+      this.nav.classList.toggle('expanded');
+    },
+  };
+  hamburger.navToggle.addEventListener('click', function (e) { hamburger.doToggle(e); });
+  // hamburger.nav.addEventListener('click', function(e) { hamburger.doToggle(e); });
+}());
+
+function burgerButtonToggle() {
+  const burgerButton = document.querySelector(".nav-toggle")
+  const yPos = window.scrollY;
+  if (yPos > 10) {
+    burgerButton.classList.add("unvisible")
+    burgerButton.classList.remove("visible");
+  } else {
+    burgerButton.classList.remove("unvisible");
+    burgerButton.classList.add("visible");
+  }
+}
+window.addEventListener("scroll", burgerButtonToggle);
+
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
