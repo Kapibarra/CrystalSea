19829a648b99c5dd600fab258a061acaeeda947f
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-05-20 14:13:04 +0300

    nav and webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -56,39 +56,6 @@ module.exports = {
                 collapseWhitespace: isProd,
             },
         }),
-        new HTMLWebpackPlugin({
-            filename: "film.html",
-            template: "film.html",
-            minify: {
-                removeComments: isProd,
-                collapseWhitespace: isProd,
-            },
-        }),
-        new HTMLWebpackPlugin({
-            filename: "price.html",
-            template: "price.html",
-            minify: {
-                removeComments: isProd,
-                collapseWhitespace: isProd,
-            },
-        }),
-        new HTMLWebpackPlugin({
-            filename: "about.html",
-            template: "about.html",
-            minify: {
-                removeComments: isProd,
-                collapseWhitespace: isProd,
-            },
-        }),
-        new HTMLWebpackPlugin({
-            filename: "contacts.html",
-            template: "contacts.html",
-            minify: {
-                removeComments: isProd,
-                collapseWhitespace: isProd,
-            },
-        }),
-
         new MiniCssExtractPlugin({
             filename: filename("css"),
         }),
