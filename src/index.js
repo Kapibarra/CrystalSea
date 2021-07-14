00de055c6844cff83056297ab8ad19f8e805bde3
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-07-14 18:39:30 +0300

    added slider

--- src/index.js
+++ src/index.js
@@ -111,3 +111,24 @@ im.mask(selector);
 const selectorModal = document.getElementById("modalphonenum");
 const imModal = new Inputmask("+7(999)-999-99-99");
 imModal.mask(selectorModal);
+
+// slider 
+
+function slidesPlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll('.slide')
+
+  slides[activeSlide].classList.add('active')
+  
+  for (const slide of slides) {
+      slide.addEventListener('click', () => {
+          clearActiveClass()
+          slide.classList.add('active')
+      })
+  }
+  function clearActiveClass() {
+      slides.forEach((slide) => {
+  slide.classList.remove('active')
+      })
+  }
+}
+slidesPlugin(2)
