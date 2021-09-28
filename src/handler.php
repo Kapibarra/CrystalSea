43976efcb31abece29d16bec1f59293fec0eb7ba
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-09-28 14:10:12 +0300

    fix

--- src/handler.php
+++ src/handler.php
@@ -8,9 +8,9 @@ require 'php/PHPMailer-5.2.28/src/PHPMailer.php';
 require 'php/PHPMailer-5.2.28/src/SMTP.php';
 
 $mail = new PHPMailer(true);
-$mail_subject = 'Subject';
-$mail_to_email = '***@gmail.com'; // your email
-$mail_to_name = 'Webmaster';
+$mail_subject = 'Заявка с сайта';
+$mail_to_email = 'nikita.nov2016@yandex.ru'; // your email
+$mail_to_name = 'novikovweb.ru';
 
 try {
 
@@ -21,15 +21,15 @@ try {
 
 	// Server settings
 	$mail->isSMTP(); // Send using SMTP
-	$mail->Host = 'smtp.***.com'; // Set the SMTP server to send through
+	$mail->Host = 'smtp.yandex.com'; // Set the SMTP server to send through
 	$mail->SMTPAuth = true; // Enable SMTP authentication
-	$mail->Username = '***'; // SMTP username
-	$mail->Password = '***'; // SMTP password
+	$mail->Username = 'noviweb@yandex.ru'; // SMTP username
+	$mail->Password = 'cf911313'; // SMTP password
 	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 	$mail->Port = 465; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-	$mail->setFrom($mail_to_email, $mail_to_name); // Your email
-	$mail->addAddress($mail_from_email, $mail_from_name); // Add a recipient
+	$mail->setFrom($mail->Username, 'Aquaroom.ru'); // Your email
+	$mail->addAddress($mail_to_email, $mail_to_name); // Add a recipient на какой емейл отправить!! проверить!!!!
 
 	for($ct=0; $ct<count($_FILES['file_attach']['tmp_name']); $ct++) {
 		$mail->AddAttachment($_FILES['file_attach']['tmp_name'][$ct], $_FILES['file_attach']['name'][$ct]);
@@ -44,7 +44,6 @@ try {
 		<strong>Name:</strong> ' . $mail_from_name . '<br>
 		<strong>Email:</strong> ' . $mail_from_email . '<br>
 		<strong>Message:</strong> ' . $mail_message;
-
 	$mail->Send();
 
 	echo 'Message has been sent';
